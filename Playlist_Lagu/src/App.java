def23c5852921e5299c317cfea7247ea663bf446
Asyajj Abdul Qais <abdulqois20@gmail.com>
2025-10-07 13:34:27 +0700

    Add files via upload

---
+++ Playlist_Lagu/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        Playlist myPlaylist = new Playlist();
+        myPlaylist.addSong("Cicak Cicak di Dinding");
+        myPlaylist.addSong("Aku Anak Pintar");
+        myPlaylist.addSong("Mars TIF");
+
+        myPlaylist.displayPlaylist();
+    }
+}
